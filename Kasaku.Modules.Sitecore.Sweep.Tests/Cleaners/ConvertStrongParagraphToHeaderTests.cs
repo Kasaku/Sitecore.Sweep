3ca4d0b759c503f8ed72613b9db1c2e48409f9f1
Alex Washtell <alex@kasaku.co.uk>
2020-01-13 21:03:03 +0000

    Additional tests.

--- Kasaku.Modules.Sitecore.Sweep.Tests/Cleaners/ConvertStrongParagraphToHeaderTests.cs
+++ Kasaku.Modules.Sitecore.Sweep.Tests/Cleaners/ConvertStrongParagraphToHeaderTests.cs
@@ -30,6 +30,30 @@ namespace Kasaku.Modules.Sitecore.Sweep.Tests.Cleaners
 			tester.AssertParsedHtml("<p>This is a regular paragraph</p>", "<p>This is a regular paragraph</p>");
 		}
 
+        [Test]
+        public void DefaultReplacementWithMultipleInstances()
+        {
+            var tester = new CleanerTester<ConvertStrongParagraphToHeader>();
+
+            tester.AssertParsedHtml("<h4>This is already a header!</h4><p><strong>This is really a header!</strong></p>", "<h4>This is already a header!</h4><h4>This is really a header!</h4>");
+        }
+
+        [Test]
+        public void DefaultReplacementWithMultipleInstancesSomeAlreadyFixed()
+        {
+            var tester = new CleanerTester<ConvertStrongParagraphToHeader>();
+
+            tester.AssertParsedHtml("<p><strong>This is really a header!</strong></p><p><strong>This is really a header!</strong></p>", "<h4>This is really a header!</h4><h4>This is really a header!</h4>");
+        }
+
+		[Test]
+        public void DefaultReplacementWithMultipleMixedInstances()
+        {
+            var tester = new CleanerTester<ConvertStrongParagraphToHeader>();
+
+            tester.AssertParsedHtml("<p><strong>This is really a header!</strong></p><p>This is a regular paragraph</p><p><strong>This is really a header!</strong></p>", "<h4>This is really a header!</h4><p>This is a regular paragraph</p><h4>This is really a header!</h4>");
+        }
+
 		[Test]
 		public void CustomReplacement()
 		{
